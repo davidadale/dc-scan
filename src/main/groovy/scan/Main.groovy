8f2cd316a13b587021089d60dc40f9c30839210d
David Dale <davidadale@gmail.com>
2013-03-07 08:58:39 -0500

    fixes to build script and added the order verification call

--- src/main/groovy/scan/Main.groovy
+++ src/main/groovy/scan/Main.groovy
@@ -1,6 +1,9 @@
 package scan
 
 import static groovy.io.FileType.FILES
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.*
+import static groovyx.net.http.Method.*
 
 /*
 	def dir = new File('/home/groovy/projects/test/src')
@@ -37,6 +40,47 @@ public class Main{
 		    //}
 		}	
 	}
+	
+	protected void verifyOrder(String orderNumber){
+        def http = new HTTPBuilder( 'http://localhost:9000' )
+
+        // perform a GET request, expecting JSON response data
+        http.request( GET, JSON ) {
+          uri.path = "/api/order/${orderNumber}"
+          headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+
+          // response handler for a success response code:
+          response.success = { resp, json ->
+            println resp.statusLine
+            println json
+            if(json.success == true){
+                
+                
+                
+                
+                
+                
+                println "======================================================================="                
+                println "  ___      _          ___ _             _              "
+                println " |   \\ _ _(_)_ _____ / __| |___ __ _ __| |___ _ _ ___  "
+                println " | |) | '_| \\ V / -_) (__| / -_) _` / _` / -_) '_(_-<  "
+                println " |___/|_| |_|\\_/\\___|\\___|_\\___\\__,_\\__,_\\___|_| /__/  "
+                println ""
+                println json.data.customer.firstName
+                println json.data.customer.lastName                
+            }else{
+                println "FAIL!"
+            }
+
+          }
+
+          // handler for any failure status code:
+          response.failure = { resp ->
+            println "Unexpected error: ${resp.statusLine.statusCode} : ${resp.statusLine.reasonPhrase}"
+          }
+        }	    
+	}
+	
 	protected String getVersion(){
 		return "0.1-alpha"
 	}
@@ -67,10 +111,11 @@ public class Main{
 
 	      if (opt.h){
 	 		cli.usage()
-	      }else if(opt.o && opt.p){
-			main.startScan()
+	      }else if(opt.o){
+			//main.startScan()
 			println opt.o 
-			println opt.p
+			main.verifyOrder( opt.o )
+			//println opt.p
 		  }else if(opt.v){
 			println main.version
 		  }else{
